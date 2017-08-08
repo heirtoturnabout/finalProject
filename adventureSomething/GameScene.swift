87efc218fafc0f72607dc21620a946b2f42c3f7e
iD Student <idstudent@ids-imac.local>
2017-08-08 16:54:22 -0400

    started on final project. added sprites and two screens. needs more work....

--- adventureSomething/GameScene.swift
+++ adventureSomething/GameScene.swift
@@ -14,27 +14,41 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    let heroStanding = SKSpriteNode(imageNamed: "FFhero")
+    let heroBackFacingPlayer = SKSpriteNode(imageNamed: "FFhero_4")
+    let heroStandingRight = SKSpriteNode(imageNamed: "FFhero_6")
+    let heroStandingLeft = SKSpriteNode(imageNamed: "FFhero_2")
+    let heroWalkingRight = SKSpriteNode(imageNamed: "FFhero_5")
+    let heroWalkingLeft = SKSpriteNode(imageNamed: "FFhero_3")
+    
+    let castleDoor = SKSpriteNode(imageNamed: "door")
+    
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        backgroundColor = SKColor.gray
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let xCoord = size.width * 0.5
+        let yCoord = size.width * 0.5
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        heroStanding.size.width = 60
+        heroStanding.size.height = 60
+        heroBackFacingPlayer.size.width = 60
+        heroBackFacingPlayer.size.height = 60
+        heroStandingLeft.size.width = 60
+        heroStandingLeft.size.height = 60
+        heroStandingRight.size.width = 60
+        heroStandingRight.size.height = 60
+        heroWalkingLeft.size.width = 60
+        heroWalkingRight.size.height = 60
+        
+        castleDoor.size.height = 70
+        castleDoor.size.width = 70
+        
+        heroBackFacingPlayer.position = CGPoint (x: xCoord, y: yCoord)
+        castleDoor.position = CGPoint (x: xCoord, y: yCoord + 400)
+        addChild(heroBackFacingPlayer)
+        addChild(castleDoor)
     }
     
     
@@ -63,11 +77,7 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
--- adventureSomething/StoryController.swift
+++ adventureSomething/StoryController.swift
@@ -1,5 +1,5 @@
 //
-//  GameViewController.swift
+//  StoryController.swift
 //  adventureSomething
 //
 //  Created by iD Student on 8/8/17.
@@ -7,36 +7,18 @@
 //
 
 import UIKit
-import SpriteKit
-import GameplayKit
 
-class GameViewController: UIViewController {
-
+class StoryController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            let scene = GameScene (size: view.bounds.size)
-
-                // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
-                
-                // Present the scene
-            view.presentScene(scene)
-            
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -44,12 +26,12 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
